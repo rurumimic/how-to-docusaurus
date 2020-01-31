12878f29ce808ad66c27386aee7f576bcee6a2e1
Dohyun Kim <unhyop@gmail.com>
2020-01-31 09:27:37 +0900

    사이드바 카테고리 형식 편집

--- sidebars.js
+++ sidebars.js
@@ -9,11 +9,12 @@ const fs = require('fs');
 const path = require('path');
 const sidebar = module.exports = {}; // 사이드바 모듈 생성
 const category = (label, items) => ({ type: 'category', label: label, items: items }); // 카테고리 빌더
-
-const docs = (prefix, items) => { // [`폴더/경로/문서`... ] 빌더
-  if (typeof items !== 'undefined') return items.map(item => prefix.concat(item)); // 경로 입력 간소화
-  else return docs(prefix, fs.readdirSync(path.join(__dirname, 'docs', prefix)).map(file => path.parse(file).name)); // 지정 폴더 카테고리 자동화
+const docs = (dirs, items) => {
+  const prefix = path.join(...dirs);
+  if (typeof items !== 'undefined') return items.map(item => path.join(prefix, item)); // 경로 입력 간소화
+  else return docs([prefix], fs.readdirSync(path.join(__dirname, 'docs', prefix)).map(file => path.parse(file).name)); // 지정 폴더 카테고리 자동화
 }
+let prefix = ''; // 폴더 경로
 
 sidebar.someSidebar = {
   Docusaurus: ['doc1', 'doc2', 'doc3'],
@@ -21,6 +22,7 @@ sidebar.someSidebar = {
   Category: [],
 }
 
+prefix = ''
 sidebar.someSidebar['Category'] = [
-  category('하위 카테고리', docs('')),
+  category('하위 카테고리', docs([prefix])),
 ]
